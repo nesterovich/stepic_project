ec6daae00fd3a86a67522c2b4f206c7292f8aec3
ekaterina.nesterovich <ekaterina337471@gmail.com>
2016-11-18 01:42:15 +0200

    26

--- init.sh
+++ init.sh
@@ -2,4 +2,3 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
  sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
